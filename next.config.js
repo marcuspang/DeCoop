e917874986afde06d7f1e277fae12c90cf1d9760
Marcus Pang <marcuspangyuyang@gmail.com>
2022-10-08 17:18:37 +0800

    Fix .env file

--- next.config.js
+++ next.config.js
@@ -2,12 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  env: {
-    APP_DOMAIN: process.env.APP_DOMAIN,
-    MORALIS_API_KEY: process.env.MORALIS_API_KEY,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-  },
+  env: {},
 };
 
 module.exports = nextConfig;
