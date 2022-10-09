a28a9e97c113edfbfa32c11ed7248950e61609d4
Marcus Pang <marcuspangyuyang@gmail.com>
2022-10-09 13:07:17 +0800

    Add missing provider to events API

--- pages/api/events.ts
+++ pages/api/events.ts
@@ -3,20 +3,28 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 const abi = [
   "event Withdraw(address indexed to, uint256 amount)",
-  "event Deposit(address indexed from, uint256 amount)"
+  "event Deposit(address indexed from, uint256 amount)",
 ];
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-    const community: string = req.body['community'];
-    const address: string = req.body['address'] || null;
+  const community: string = req.body["community"];
+  const address: string = req.body["address"] || null;
 
-    const communityContract = new ethers.Contract(community, abi);
+  const communityContract = new ethers.Contract(
+    community,
+    abi,
+    ethers.getDefaultProvider("goerli")
+  );
 
-    const deposits = communityContract.queryFilter(communityContract.filters.Deposit(address, null));
-    const withdraws = communityContract.queryFilter(communityContract.filters.Withdraw(address, null));
+  const deposits = communityContract.queryFilter(
+    communityContract.filters.Deposit(address)
+  );
+  const withdraws = communityContract.queryFilter(
+    communityContract.filters.Withdraw(address)
+  );
 
-    res.status(200).send({ deposits, withdraws });
+  res.status(200).send({ deposits, withdraws });
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,15 @@
+import axios from "axios";
+import { useEffect } from "react";
+
 export default function Home() {
+  useEffect(() => {
+    axios
+      .post("/api/events", {
+        community: "0x0E20DE1E1dFb07EA57898368710Bb926e67f8070",
+        address: "0xAD2d2CDE7CA8d116d545099405C1FDFc57B6FD9e",
+      })
+      .then((res) => console.log(res.data));
+  }, []);
+
   return <div className="w-full px-2">Home page</div>;
 }
