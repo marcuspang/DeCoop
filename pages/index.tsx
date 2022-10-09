d8fb88513870ddcc02f1ecd7c6a38600767ab822
Marcus Pang <marcuspangyuyang@gmail.com>
2022-10-09 10:52:03 +0800

    Refactor pagination

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,7 @@
+import {
+  ArrowDownOnSquareIcon,
+  ArrowDownTrayIcon,
+} from "@heroicons/react/20/solid";
 import Head from "next/head";
 import TransactionCard from "../components/Transactions/TransactionCard";
 import TransactionChart from "../components/Transactions/TransactionChart";
@@ -5,7 +9,7 @@ import TransactionsTable from "../components/Transactions/TransactionsTable";
 
 export default function Home() {
   return (
-    <div className="lg:w-full">
+    <div className="w-full px-2">
       <Head>
         <title>Optimity</title>
         <meta name="description" content="Optimity 2022" />
@@ -28,7 +32,8 @@ export default function Home() {
       <div className="my-12 bg-white p-4 rounded shadow flex justify-between items-center">
         <h3 className="font-bold text-xl">All Transactions</h3>
         <button className="rounded border-2 p-2 border-gray-300 transition-all hover:bg-gray-100 hover:border-gray-400 focus:border-blue-600">
-          Download Full Report
+          Download Full Report{" "}
+          <ArrowDownTrayIcon className="inline mb-1" width={16} height={16} />
         </button>
       </div>
       <TransactionsTable />
